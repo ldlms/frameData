8dc92c99f2b7a60c18852387ab8514818bf376b5
Leo <leodelmas31@gmail.com>
2024-03-09 16:48:24 +0100

    format

--- controller/CharacterController.php
+++ controller/CharacterController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace frameData\controller;
 
 require './model/Character.php';
@@ -14,36 +15,31 @@ class CharacterController extends Character
     public function addCharacter()
     {
         $error = '';
-        if(isset($_POST['submit'])) {
-            if(!empty($_POST['nomCharacter']) AND isset($_FILES['imageCharacter']) AND $_FILES['imageCharacter']['error'] == UPLOAD_ERR_OK) {
+        if (isset($_POST['submit'])) {
+            if (!empty($_POST['nomCharacter']) and isset($_FILES['imageCharacter']) and $_FILES['imageCharacter']['error'] == UPLOAD_ERR_OK) {
                 $this->setNomCharacter(Utilitaire::cleanInput($_POST['nomCharacter']));
-                if($_FILES['imageCharacter']['tmp_name']!="") {
+                if ($_FILES['imageCharacter']['tmp_name'] != "") {
                     $ext = Utilitaire::getFileExtension($_FILES['imageCharacter']['name']);
-                    if($ext=='png' OR $ext=='jpg' OR $ext=='PNG' OR $ext=='JPG' OR $ext=='bmp' OR $ext=='BMP') {
+                    if ($ext == 'png' or $ext == 'jpg' or $ext == 'PNG' or $ext == 'JPG' or $ext == 'bmp' or $ext == 'BMP') {
                         $this->setImageCharacter($_FILES['imageCharacter']['name']);
-                    }else{
+                    } else {
                         $error = 'format incorrect';
                         $this->setImageCharacter('./public/images/basic.jpg');
                     }
-                }else{
+                } else {
                     $this->setImageCharacter('./public/images/basic.jpg');
                 }
-                if($this->findOneBy()) {
+                if ($this->findOneBy()) {
                     $error = 'le personnage à déja été crée';
-                }else{
+                } else {
                     $this->add();
-                    move_uploaded_file($_FILES['imageCharacter']['tmp_name'], './public/images/'.$_FILES['imageCharacter']['name']);
-                    $error = 'personnage créee !';   
+                    move_uploaded_file($_FILES['imageCharacter']['tmp_name'], './public/images/' . $_FILES['imageCharacter']['name']);
+                    $error = 'personnage créee !';
                 }
-            }else{
+            } else {
                 $error = 'veuillez remplir le formulaire';
             }
         }
         Template::render('navbar.php', 'bottom.php', 'addCharacter.php', 'addCharacter', ['script.js'], ['style.css'], $error);
     }
 }
-
-
-
-
-?>
